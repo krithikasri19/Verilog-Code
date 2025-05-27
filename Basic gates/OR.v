c9bf9a75a663f7ff48cfa820d6640104201c32c6
krithika sri <146073235+krithikasri19@users.noreply.github.com>
2025-05-27 15:50:33 +0530

    Create OR_Gate

--- Basic gates/OR.v
+++ Basic gates/OR.v
@@ -1,3 +1,34 @@
-module or_gate(input a,b, output y);
+// dataflow modelling
+
+module or_gate(
+  input a,
+  input b, 
+  output y
+);
 assign y=a|b;
+endmodule
+
+// behavioral modelling 
+
+module or_gate(
+  input a,
+  input b, 
+  output y
+);
+  always @ (*)
+    begin 
+      y = a | b;
+    end
+endmodule
+
+//structural modelling
+
+module or_gate(
+  input a,
+  input b, 
+  output y
+);
+
+  or o1 ( y,a,b);
+
 endmodule
