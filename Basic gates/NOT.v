0ec12b1ab153deeee9a6dd5c8574394ff3cfd505
krithika sri <146073235+krithikasri19@users.noreply.github.com>
2025-05-27 15:53:12 +0530

    Create NOT_gate

--- Basic gates/NOT.v
+++ Basic gates/NOT.v
@@ -1,6 +1,30 @@
+// dataflow modelling
+
 module not_gate(
   input a,
   output y
 );
   assign y = ~a;
+endmodule
+
+// behavioral modelling
+
+module not_gate(
+  input a, 
+  output y
+);
+  always @ (*)
+    begin 
+      y = ~a;
+    end
+endmodule
+
+// structural modelling
+
+module not_gate(
+  input a, 
+  output y
+);
+
+  not n1 (y,a);
 endmodule
