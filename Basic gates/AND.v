20bb816b1184b47dc703d22e757dd0f11006446c
krithika sri <146073235+krithikasri19@users.noreply.github.com>
2025-05-27 15:47:36 +0530

    Create AND_Gate

--- Basic gates/AND.v
+++ Basic gates/AND.v
@@ -1,3 +1,34 @@
-module and_gate(input a,b, output y);
-assign y= a&b;
+//Dataflow modelling
+
+module and_gate(
+  input a,
+  input b,
+  output y
+);
+
+  assign y = a & b;
+endmodule
+
+//behavioral modelling
+
+module and_gate(
+  input a,
+  input b,
+  output y
+);
+  always @ (*)
+    begin 
+      y = a & b;
+    end
+endmodule
+
+//structural modelling
+
+module and_gate(
+  input a,
+  input b,
+  output y
+);
+  and a1 (y,a,b);
+
 endmodule
