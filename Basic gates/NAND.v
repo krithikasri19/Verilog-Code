90ef24edcb63a4675e2b685a219abd581437c925
krithika sri <146073235+krithikasri19@users.noreply.github.com>
2025-05-27 15:56:17 +0530

    Create NAND_Gate

--- Basic gates/NAND.v
+++ Basic gates/NAND.v
@@ -1,7 +1,35 @@
+// dataflow modelling
+
 module nand_gate(
   input a,
   input b,
   output y
 );
   assign y = ~(a & b);
+
+endmodule
+
+// behavioral modelling
+
+module nand_gate(
+  input a,
+  input b,
+  output y
+);
+  always @ (*)
+    begin 
+      y = ~ (a & b );
+    end
+
+endmodule
+
+// structural modelling
+
+module nand_gate(
+  input a,
+  input b,
+  output y
+);
+  nand na1 (y,a,b);
+
 endmodule
