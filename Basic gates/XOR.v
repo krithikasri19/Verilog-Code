adf845574b6db54fdabb1d476f2ed11b86445250
krithika sri <146073235+krithikasri19@users.noreply.github.com>
2025-05-28 14:48:33 +0530

    Create XOR_Gate

--- Basic gates/XOR.v
+++ Basic gates/XOR.v
@@ -34,3 +34,28 @@ module xor_gate(
   xor x1 (y,a,b);
 
 endmodule
+
+//test bench
+
+module xor_tb;
+reg a,b;
+wire y;
+  xor_gate dut(.a(a),
+               .b(b),
+               .y(y)
+              );
+initial begin
+  $display("XOR GATE TRUTH TABLE");
+  $monitor("A : %b B : %b     OUT : %b ",a,b,y);
+a = 0 ; b = 0 ; #10 ;
+a = 0 ; b = 1 ; #10 ;
+a = 1 ; b = 0 ; #10 ;
+a = 1 ; b = 1 ; #10 ;
+$finish;
+end
+//TO VIEW WAVEFORMS
+initial begin
+  $dumpfile("xor_gate.vcd");
+  $dumpvars(0,xor_tb);
+end
+endmodule
