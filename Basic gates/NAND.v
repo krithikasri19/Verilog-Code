c30b54080f639d91aecc1e3f7fd24953b280e3cf
krithika sri <146073235+krithikasri19@users.noreply.github.com>
2025-05-28 14:42:14 +0530

    Create NAND_Gate

--- Basic gates/NAND.v
+++ Basic gates/NAND.v
@@ -33,3 +33,28 @@ module nand_gate(
   nand na1 (y,a,b);
 
 endmodule
+
+//test bench
+
+module nand_tb;
+reg a,b;
+wire y;
+  nand_gate dut(.a(a),
+               .b(b),
+               .y(y)
+              );
+initial begin
+  $display("NAND GATE TRUTH TABLE");
+  $monitor("A : %b B : %b     OUT : %b ",a,b,y);
+a = 0 ; b = 0 ; #10 ;
+a = 0 ; b = 1 ; #10 ;
+a = 1 ; b = 0 ; #10 ;
+a = 1 ; b = 1 ; #10 ;
+$finish;
+end
+//TO VIEW WAVEFORMS
+initial begin
+  $dumpfile("nand_gate.vcd");
+  $dumpvars(0,nand_tb);
+end
+endmodule
