db57bdb9f2c3b4ff3d4a14c7350cf21e845689f7
krithika sri <146073235+krithikasri19@users.noreply.github.com>
2025-05-28 14:46:55 +0530

    Create NOR_gate

--- Basic gates/NOR.v
+++ Basic gates/NOR.v
@@ -35,3 +35,28 @@ module nor_gate(
   nor no1 ( y,a,b);
 
 endmodule
+
+//test bench
+
+module nor_tb;
+reg a,b;
+wire y;
+  nor_gate dut(.a(a),
+               .b(b),
+               .y(y)
+              );
+initial begin
+  $display("NOR GATE TRUTH TABLE");
+  $monitor("A : %b B : %b     OUT : %b ",a,b,y);
+a = 0 ; b = 0 ; #10 ;
+a = 0 ; b = 1 ; #10 ;
+a = 1 ; b = 0 ; #10 ;
+a = 1 ; b = 1 ; #10 ;
+$finish;
+end
+//TO VIEW WAVEFORMS
+initial begin
+  $dumpfile("nor_gate.vcd");
+  $dumpvars(0,nor_tb);
+end
+endmodule
