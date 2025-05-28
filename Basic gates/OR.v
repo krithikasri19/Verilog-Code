ec868fbe2920be17edb60ae758befacfce06a51e
krithika sri <146073235+krithikasri19@users.noreply.github.com>
2025-05-28 14:39:22 +0530

    Create OR_Gate

--- Basic gates/OR.v
+++ Basic gates/OR.v
@@ -32,3 +32,28 @@ module or_gate(
   or o1 ( y,a,b);
 
 endmodule
+
+//test bench
+
+module or_tb;
+reg a,b;
+wire y;
+  or_gate dut(.a(a),
+               .b(b),
+               .y(y)
+              );
+initial begin
+$display("AND GATE TRUTH TABLE");
+  $monitor("A : %b B : %b     OUT : %b ",a,b,y);
+a = 0 ; b = 0 ; #10 ;
+a = 0 ; b = 1 ; #10 ;
+a = 1 ; b = 0 ; #10 ;
+a = 1 ; b = 1 ; #10 ;
+$finish;
+end
+//TO VIEW WAVEFORMS
+initial begin
+  $dumpfile("or_gate.vcd");
+  $dumpvars(0,or_tb);
+end
+endmodule
